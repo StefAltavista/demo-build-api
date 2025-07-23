b21144c2767d3feb01f2e4633faf001caa683540
Pablo Bueno <paulobu3n0@gmail.com>
2025-07-23 22:45:36 +0900

    demo build

---
+++ api/controllers/checkAPI.js
@@ -0,0 +1,9 @@
+export const checkAPI = async (req, res) => {
+ try {
+  return res.status(200).json({ status: "OK" })
+ } catch (error) {
+  return res.status(500).json({ status: "unavailable" })
+ }
+}
+
+
---
+++ app/transactions/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Page() {
+  return (
+  <div className="max-w-7xl mx-auto px-4 py-6">
+   {/* Header */}
+   <div className="text-center mb-8">
+    <h1 className="text-2xl font-bold">Transactions</h1>
+   </div>
+  </div>
+ )
+}
